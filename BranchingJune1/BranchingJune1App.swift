d0f30e0b84a8ca068935a89375af1f5c98f5c82f
ctaicher <cltaicher@gmail.com>
2024-06-01 12:58:42 -0700

    Initial Commit

---
+++ BranchingJune1/BranchingJune1App.swift
@@ -0,0 +1,17 @@
+//
+//  BranchingJune1App.swift
+//  BranchingJune1
+//
+//  Created by Christopher Taicher on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BranchingJune1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
